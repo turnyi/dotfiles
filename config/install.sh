a4923ef2c03b7d369491b2e6ce65b064fc4d739d
402Martin <402martin@gmail.com>
2023-04-22 22:19:08 -0300

    adeded changes

--- config/install.sh
+++ config/install.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-# Intsall link of home files
+# Install link of home files
 rm -rf ~/scripts ~/zsh ~/.tmux.conf ~/.zprofile ~/.zshrc ~/.DS_Store ~/Notes
 stow -t ~ home
 
-# Install config files
+# # Install config files
 # rm -rf ~/.config
 # mkdir ~/.config && stow -t ~/.config .config
