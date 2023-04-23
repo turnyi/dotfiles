4e97b319544fb5da8c5e37f80a768507d0a74380
402Martin <402martin@gmail.com>
2023-04-22 22:01:52 -0300

    added new configs

--- config/install.sh
+++ config/install.sh
@@ -7,5 +7,5 @@ rm -rf ~/scripts ~/zsh ~/.tmux.conf ~/.zprofile ~/.zshrc ~/.DS_Store ~/Notes
 stow -t ~ home
 
 # Install config files
-rm -rf ~/.config
-mkdir ~/.config && stow -t ~/.config .config
+# rm -rf ~/.config
+# mkdir ~/.config && stow -t ~/.config .config
