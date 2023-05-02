b4ffd3ce8a630904dc9f5f82758a1fb64fd65082
402Martin <402martin@gmail.com>
2023-05-01 21:49:45 -0300

    autoformatter now goes to normal mode after save

--- config/.config/nvim/lua/plugins/init.lua
+++ config/.config/nvim/lua/plugins/init.lua
@@ -1,16 +1,30 @@
-require('plugins.lsp')
-require('plugins.telescope')
-require('plugins.null-ls')
-require('plugins.nvim-cmp')
-require('plugins.trouble')
-require('plugins.bufferline')
-vim.api.nvim_exec([[
+require("plugins.lsp")
+require("plugins.telescope")
+require("plugins.null-ls")
+require("plugins.nvim-cmp")
+require("plugins.trouble")
+require("plugins.bufferline")
+
+vim.api.nvim_exec(
+	[[
 augroup AutoFormat
   autocmd!
-  autocmd BufWritePre * lua vim.lsp.buf.format()
+  autocmd BufWritePre * lua vim.lsp.buf.format(); vim.cmd('stopinsert')
 augroup END
-]], true)
+]],
+	true
+)
 
 -- Add a keymapping to open the diagnostic float
-vim.api.nvim_set_keymap('n', 'g a', ':lua vim.diagnostic.open_float({scope="line"})<CR>',
-  { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"g a",
+	':lua vim.diagnostic.open_float({scope="line"})<CR>',
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"g a",
+	':lua vim.diagnostic.open_float({scope="line"})<CR>',
+	{ noremap = true, silent = true }
+)
