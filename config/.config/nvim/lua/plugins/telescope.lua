f49832270e3dca7dc4b0517addb832edc8e5a191
402Martin <402martin@gmail.com>
2023-05-18 09:05:49 -0300

    rm .git as ignored pattermn

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -1,127 +1,143 @@
 local is_telescope_installed, telescope = pcall(require, "telescope")
 local is_trouble_installed, trouble = pcall(require, "trouble.providers.telescope")
 local is_action_layout, action_layout = pcall(require, "telescope.actions.layout")
-local fb_actions = require "telescope._extensions.file_browser.actions"
-local actions = require('telescope.actions')
-if not is_telescope_installed then return end
-if not is_trouble_installed then return end
-if not is_action_layout then return end
+local fb_actions = require("telescope._extensions.file_browser.actions")
+local actions = require("telescope.actions")
+if not is_telescope_installed then
+	return
+end
+if not is_trouble_installed then
+	return
+end
+if not is_action_layout then
+	return
+end
 
 telescope.setup({
-    defaults = {
-        vimgrep_arguments = {
-            "rg", "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case"
-        },
-        mappings = {
-            i = { 
-            ["?"] = action_layout.toggle_preview, 
-            ['<C-d>'] = actions.delete_buffer,
-          },
-            n = { 
-            ["?"] = action_layout.toggle_preview, 
-            ['<C-d>'] = actions.delete_buffer,
-          },
-
-        },
-        prompt_prefix = " ",
-        selection_caret = " ",
-        entry_prefix = "  ",
-        initial_mode = "insert",
-        selection_strategy = "reset",
-        sorting_strategy = "ascending",
-        layout_strategy = "horizontal",
-        layout_config = {
-            horizontal = { prompt_position = "top", preview_width = 0.55, results_width = 0.8 },
-            vertical = { mirror = false },
-            width = 0.87,
-            height = 0.80,
-            preview_cutoff = 240
-        },
-        file_sorter = require("telescope.sorters").get_fuzzy_file,
-        file_ignore_patterns = { "node_modules", ".git", "plugged" },
-        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-        path_display = { "truncate" },
-        winblend = 0,
-        -- border = {},
-        -- borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-        color_devicons = true,
-        use_less = true,
-        extensions = {
-            fzf = { fuzzy = true, override_generic_sorter = true, override_file_sorter = true, case_mode = "smart_case" },
-            file_browser = {
-            -- path
-            -- cwd
-            cwd_to_path = true,
-            grouped = false,
-            files = true,
-            add_dirs = true,
-            depth = 1,
-            auto_depth = false,
-            select_buffer = false,
-            hidden = false,
-            -- respect_gitignore
-            -- browse_files
-            -- browse_folders
-            hide_parent_dir = false,
-            collapse_dirs = false,
-            prompt_path = false,
-            quiet = false,
-            dir_icon = "",
-            dir_icon_hl = "Default",
-            display_stat = { date = true, size = true, mode = true },
-            hijack_netrw = false,
-            use_fd = true,
-            git_status = true,
-            mappings = {
-                ["i"] = {
-                ["<A-c>"] = fb_actions.create,
-                ["<S-CR>"] = fb_actions.create_from_prompt,
-                ["<A-r>"] = fb_actions.rename,
-                ["<A-m>"] = fb_actions.move,
-                ["<A-y>"] = fb_actions.copy,
-                ["<A-d>"] = fb_actions.remove,
-                ["<C-o>"] = fb_actions.open,
-                ["<C-g>"] = fb_actions.goto_parent_dir,
-                ["<C-e>"] = fb_actions.goto_home_dir,
-                ["<C-w>"] = fb_actions.goto_cwd,
-                ["<C-t>"] = fb_actions.change_cwd,
-                ["<C-f>"] = fb_actions.toggle_browser,
-                ["<C-h>"] = fb_actions.toggle_hidden,
-                ["<C-s>"] = fb_actions.toggle_all,
-                ["<bs>"] = fb_actions.backspace,
-                },
-                ["n"] = {
-                ["c"] = fb_actions.create,
-                ["r"] = fb_actions.rename,
-                ["m"] = fb_actions.move,
-                ["y"] = fb_actions.copy,
-                ["d"] = fb_actions.remove,
-                ["o"] = fb_actions.open,
-                ["g"] = fb_actions.goto_parent_dir,
-                ["e"] = fb_actions.goto_home_dir,
-                ["w"] = fb_actions.goto_cwd,
-                ["t"] = fb_actions.change_cwd,
-                ["f"] = fb_actions.toggle_browser,
-                ["h"] = fb_actions.toggle_hidden,
-                ["s"] = fb_actions.toggle_all,
-                },
-            },
-            },
-        },
-        set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-        -- Developer configurations: Not meant for general override
-        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker
-    },
-     pickers = {
-        live_grep = {
-            additional_args = function(opts)
-                return {"--hidden"}
-            end
-        },
-    },
+	defaults = {
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+		},
+		mappings = {
+			i = {
+				["?"] = action_layout.toggle_preview,
+				["<C-d>"] = actions.delete_buffer,
+			},
+			n = {
+				["?"] = action_layout.toggle_preview,
+				["<C-d>"] = actions.delete_buffer,
+			},
+		},
+		prompt_prefix = " ",
+		selection_caret = " ",
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "ascending",
+		layout_strategy = "horizontal",
+		layout_config = {
+			horizontal = { prompt_position = "top", preview_width = 0.55, results_width = 0.8 },
+			vertical = { mirror = false },
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 240,
+		},
+		file_sorter = require("telescope.sorters").get_fuzzy_file,
+		file_ignore_patterns = { "node_modules", "plugged" },
+		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+		path_display = { "truncate" },
+		winblend = 0,
+		-- border = {},
+		-- borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+		color_devicons = true,
+		use_less = true,
+		extensions = {
+			fzf = {
+				fuzzy = true,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "smart_case",
+			},
+			file_browser = {
+				-- path
+				-- cwd
+				cwd_to_path = true,
+				grouped = false,
+				files = true,
+				add_dirs = true,
+				depth = 1,
+				auto_depth = false,
+				select_buffer = false,
+				hidden = true,
+				-- respect_gitignore
+				-- browse_files
+				-- browse_folders
+				hide_parent_dir = false,
+				collapse_dirs = false,
+				prompt_path = false,
+				quiet = false,
+				dir_icon = "",
+				dir_icon_hl = "Default",
+				display_stat = { date = true, size = true, mode = true },
+				hijack_netrw = false,
+				use_fd = true,
+				git_status = true,
+				mappings = {
+					["i"] = {
+						["<A-c>"] = fb_actions.create,
+						["<S-CR>"] = fb_actions.create_from_prompt,
+						["<A-r>"] = fb_actions.rename,
+						["<A-m>"] = fb_actions.move,
+						["<A-y>"] = fb_actions.copy,
+						["<A-d>"] = fb_actions.remove,
+						["<C-o>"] = fb_actions.open,
+						["<C-g>"] = fb_actions.goto_parent_dir,
+						["<C-e>"] = fb_actions.goto_home_dir,
+						["<C-w>"] = fb_actions.goto_cwd,
+						["<C-t>"] = fb_actions.change_cwd,
+						["<C-f>"] = fb_actions.toggle_browser,
+						["<C-h>"] = fb_actions.toggle_hidden,
+						["<C-s>"] = fb_actions.toggle_all,
+						["<bs>"] = fb_actions.backspace,
+					},
+					["n"] = {
+						["c"] = fb_actions.create,
+						["r"] = fb_actions.rename,
+						["m"] = fb_actions.move,
+						["y"] = fb_actions.copy,
+						["d"] = fb_actions.remove,
+						["o"] = fb_actions.open,
+						["g"] = fb_actions.goto_parent_dir,
+						["e"] = fb_actions.goto_home_dir,
+						["w"] = fb_actions.goto_cwd,
+						["t"] = fb_actions.change_cwd,
+						["f"] = fb_actions.toggle_browser,
+						["h"] = fb_actions.toggle_hidden,
+						["s"] = fb_actions.toggle_all,
+					},
+				},
+			},
+		},
+		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		-- Developer configurations: Not meant for general override
+		buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+	},
+	pickers = {
+		live_grep = {
+			additional_args = function(opts)
+				return { "--hidden" }
+			end,
+		},
+	},
 })
 
 telescope.load_extension("fzf")
@@ -129,4 +145,3 @@ telescope.load_extension("zk")
 telescope.load_extension("file_browser")
 return telescope
 -- Telescope file_browser cwd=%:p:h
-
