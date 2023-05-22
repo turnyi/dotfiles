cb33be191347a5bb626538d6fc07aa50db637e07
402Martin <402martin@gmail.com>
2023-05-22 20:52:44 -0300

    added .sh scripts to enter tmux

---
+++ config/home/scripts/hexec.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ROOT=$(dirname $(readlink -f $(which "$0")))
+
+source $ROOT/print.sh
+
+[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
+
+fileLines=()
+while IFS=";" read -u3 var val; do
+  fileLines=("$val" "${fileLines[@]}")
+done 3<$HISTFILE
+
+command=$(printf "%s\n" "${fileLines[@]}" | fzf-tmux -p 80%,60%)
+
+echo $(green "Executing >>>") $(yellow "$command") $(green "<<< ...")
+eval $command
