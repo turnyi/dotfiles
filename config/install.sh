d190208a728b9821d831e964e9199c175794141c
402Martin <402martin@gmail.com>
2023-05-22 23:02:38 -0300

    Added tmuxinator configs

--- config/install.sh
+++ config/install.sh
@@ -9,8 +9,9 @@ echo "$(pwd)"
 stow -t ~ home
 
 # Install config files
-rm -rf ~/.config/ohmyposh ~/.config/nvim ~/.config/gh 
+rm -rf ~/.config/ohmyposh ~/.config/nvim ~/.config/gh ~/.config/tmuxinator 
 mkdir -p .config && cd .config &&  mkdir ~/.config/ohmyposh && stow -t ~/.config/ohmyposh ohmyposh 
 mkdir ~/.config/nvim && stow -t ~/.config/nvim nvim
 mkdir ~/.config/gh && stow -t ~/.config/gh gh
+mkdir ~/.config/tmuxinator && stow -t ~/.config/tmuxinator tmuxinator
 
