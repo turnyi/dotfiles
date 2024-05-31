47913f55005c5595e4e19e47ef429ab8e3f60773
402Martin <402martin@gmail.com>
2024-05-31 14:30:04 -0300

    updated mappings

--- config/home/scripts/commit-branch-name.sh
+++ config/home/scripts/commit-branch-name.sh
@@ -5,4 +5,5 @@ BRANCH_NAME=$(git branch --show-current)
 BRANCH_CARD_ID=$(echo "$BRANCH_NAME" | sed -n 's/\(ORIG-[0-9]*\).*/[\1]/p')
 CARD_TITLE=$(echo "$BRANCH_NAME" | sed 's/ORIG-[0-9]*/ /;s/-/ /g;s/  */ /g')
 
+echo $BRANCH_CARD_ID$CARD_TITLE
 git commit -m "$BRANCH_CARD_ID$CARD_TITLE"
