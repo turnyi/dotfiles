59067da97a38fa8f444b2d52f83d099e410ae8d3
turny <402martin@gmailcom>
2025-05-16 12:11:48 -0300

    added layout specific confit depending on moniotr

--- config/.config/hypr/workspace_layout_resoultion.sh
+++ config/.config/hypr/workspace_layout_resoultion.sh
@@ -1,23 +1,14 @@
-
 #!/usr/bin/env bash
 
-# Target width of Samsung G9
-TARGET_WIDTH=5120
-LAYOUT_MASTER="master"
-LAYOUT_OTHER="dwindle"
+master_layout_min_width=5120
 
-socat - UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do
-    if [[ "$line" =~ ^workspace>> ]]; then
-        workspace=$(hyprctl activeworkspace -j | jq -r '.id')
-        monitor=$(hyprctl activeworkspace -j | jq -r '.monitor')
+monitor_info=$(hyprctl monitors -j)
+active_width=$(echo "$monitor_info" | jq -r '.[] | select(.focused == true) | .width')
 
-        monitor_info=$(hyprctl monitors -j | jq -r ".[] | select(.name == \"$monitor\")")
-        width=$(echo "$monitor_info" | jq -r '.width')
-
-        if [[ "$width" == "$TARGET_WIDTH" ]]; then
-            hyprctl dispatch layoutmsg "$LAYOUT_MASTER:workspace $workspace"
-        else
-            hyprctl dispatch layoutmsg "$LAYOUT_OTHER:workspace $workspace"
-        fi
-    fi
-done
+if [ "$active_width" -ge $master_layout_min_width ]; then
+  hyprctl keyword general:layout master
+  echo "Set layout to MASTER (width = $active_width)"
+else
+  hyprctl keyword general:layout dwindle
+  echo "Set layout to DWINDLE (width = $active_width)"
+fi
